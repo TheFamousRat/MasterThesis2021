c697f9c441cae26ab795c1e35b307fe15fc49965
TheFamousRat <minone.roy@free.fr>
2021-02-09 17:25:53 +0100

    Base for working with pmp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
-#include <core/box.h>
+// Copyright 2013-2017 the Polygon Mesh Processing Library developers.
+// Distributed under a MIT-style license, see LICENSE.txt for details.
 
-int main() {
-    std::cout << "Yo\n";
-    return 0;
+#include <pmp/SurfaceMesh.h>
+#include <iostream>
+
+using namespace pmp;
+
+int main(int argc, char** argv)
+{
+    //! [iterators]
+
+    SurfaceMesh mesh;
+
+    if (argc > 1)
+        mesh.read(argv[1]);
+
+    float mean_valence = 0.0f;
+
+    // loop over all vertices
+    for (auto vit : mesh.vertices())
+    {
+        // sum up vertex valences
+        mean_valence += mesh.valence(vit);
+    }
+
+    std::cout << "Brooks was here\n";
+    mean_valence /= mesh.n_vertices();
+
+    std::cout << "mean valence: " << mean_valence << std::endl;
+
+    //! [iterators]
 }
